35bbebde1c7ad95f16769c249f2657751498db41
Abiud Sokomi <abiudsokomi@gmail.com>
2024-01-04 18:45:02 +0300

    my commit

---
+++ index.js
@@ -0,0 +1,15 @@
+
+const PizzaShop = require("./pizza-shop");
+const DrinkMachine = require("./drink-machine");
+
+const pizzaShop = new PizzaShop();
+const drinkMachine = new DrinkMachine();
+
+pizzaShop.on("order", (size, topping) => {
+    console.log("Order received! Baking a ${size} pizza with ${topping}");
+    drinkMachine.serveDrink(size);
+});
+pizzaShop.order("large", "mushrooms");
+pizzaShop.displayOrderNumber();
+
+
